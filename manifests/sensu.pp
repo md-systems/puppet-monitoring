760555388f3dedfcb9db56be84b01ff5c039f192
corvus_ch <christian.haeusler@md-systems.ch>
2014-03-04 23:16:51 +0100

    Enable amqp exchange from sensu to graphite

--- manifests/sensu.pp
+++ manifests/sensu.pp
@@ -23,7 +23,8 @@
 class monitoring::sensu (
   $rabbitmq_password,
   $rabbitmq_vhost = '/sensu',
-  $rabbitmq_user = 'sensu'
+  $rabbitmq_user = 'sensu',
+  $rabbitmq_exchange = 'metrics'
 ) {
   class {'::redis': }
   class {'::rabbitmq':
@@ -45,7 +46,14 @@ class monitoring::sensu (
     write_permission     => '.*',
   }
 
-  class { '::graphite': }
+  class { '::graphite':
+    gr_amqp_enable              => true,
+    gr_amqp_vhost               => $rabbitmq_vhost,
+    gr_amqp_user                => $rabbitmq_user,
+    gr_amqp_password            => $rabbitmq_password,
+    gr_amqp_exchange            => $rabbitmq_exchange,
+    gr_amqp_metric_name_in_body => true,
+  }
 
   include ::monitoring::sensu::install
 
@@ -65,6 +73,15 @@ class monitoring::sensu (
   Class['::redis']->Class['::sensu']
   Class['::rabbitmq']->Class['::sensu']
   Class['::monitoring::sensu::install']->Class['::sensu']
+
+  sensu::handler { 'graphite':
+    type     => 'amqp',
+    exchange => {
+      'type'    => 'topic',
+      'name'    => $rabbitmq_exchange,
+      'durable' => true
+    },
+  }
 
   Sensu::Check {
     handlers     => 'default',
