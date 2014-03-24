309ff5a980dc9b940c8fa416f14c75758487358d
corvus_ch <christian.haeusler@md-systems.ch>
2014-03-24 12:34:42 +0100

    Disable broken munin config collects

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,7 @@ class monitoring (
   $companies = [],
   $locations = []
 ) {
-  $companies.foreach {|$company| File <<| tag == "munin-node-${company}" |>> }
-  $locations.foreach {|$location| File <<| tag == "munin-node-${location}" |>> }
-  $companies.foreach {|$company| $locations.foreach {|$location| File <<| tag == "munin-node-${company}-${location}" |>> }}
+  #$companies.foreach {|$company| File <<| tag == "munin-node-${company}" |>> }
+  #$locations.foreach {|$location| File <<| tag == "munin-node-${location}" |>> }
+  #$companies.foreach {|$company| $locations.foreach {|$location| File <<| tag == "munin-node-${company}-${location}" |>> }}
 }
