4adc30eec64e442566b7d2609affb37b374f3f5b
corvus_ch <christian.haeusler@md-systems.ch>
2014-04-03 16:47:26 +0200

    Add node selection to puppet manifest

--- vagrant.pp
+++ vagrant.pp
@@ -1,25 +1,3 @@
-package { 'python-pip':
-  ensure => present,
-  require  => Package['python-dev'],
-}
-
-package { 'python-dev':
-  ensure => present,
-  require  => Package['build-essential'],
-}
-
-package { 'twisted':
-  ensure   => '11.1.0',
-  provider => pip,
-  require  => Package['python-pip'],
-}
-
-package { 'txamqp':
-  ensure   => present,
-  provider => pip,
-  require  => Package['python-pip'],
-}
-
 package { 'build-essential':
   ensure => present,
 }
@@ -29,24 +7,49 @@ package { 'ruby-dev':
   require  => Package['build-essential'],
 }
 
-class { monitoring::server:
-  plugins => [
-    'puppet:///modules/monitoring/sensu/plugins/check-cpu.rb',
-    'puppet:///modules/monitoring/sensu/plugins/check-procs.rb',
-    'puppet:///modules/monitoring/sensu/plugins/cpu-metrics.rb'
-  ],
-  checks => {
-    check_cron => {
-      command => '/etc/sensu/plugins/check-procs.rb -p crond -C 1'
-    },
-    check_cpu => {
-      command => '/etc/sensu/plugins/check-cpu.rb'
-    },
-    cpu_metrics => {
-      command => '/etc/sensu/plugins/cpu-metrics.rb',
-      type => 'metric',
-      handlers => ['graphite'],
-      interval => 10
+node 'monitoring.example.com' {
+
+  package { 'python-pip':
+    ensure => present,
+    require  => Package['python-dev'],
+  }
+
+  package { 'python-dev':
+    ensure => present,
+    require  => Package['build-essential'],
+  }
+
+  package { 'twisted':
+    ensure   => '11.1.0',
+    provider => pip,
+    require  => Package['python-pip'],
+  }
+
+  package { 'txamqp':
+    ensure   => present,
+    provider => pip,
+    require  => Package['python-pip'],
+  }
+
+  class { monitoring::server:
+    plugins => [
+      'puppet:///modules/monitoring/sensu/plugins/check-cpu.rb',
+      'puppet:///modules/monitoring/sensu/plugins/check-procs.rb',
+      'puppet:///modules/monitoring/sensu/plugins/cpu-metrics.rb'
+    ],
+    checks => {
+      check_cron => {
+        command => '/etc/sensu/plugins/check-procs.rb -p crond -C 1'
+      },
+      check_cpu => {
+        command => '/etc/sensu/plugins/check-cpu.rb'
+      },
+      cpu_metrics => {
+        command => '/etc/sensu/plugins/cpu-metrics.rb',
+        type => 'metric',
+        handlers => ['graphite'],
+        interval => 10
+      }
     }
   }
 }
